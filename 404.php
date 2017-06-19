9e08a259823385af5bc72b91c1c944411c79d4df
Saigesp <saigesp@saigesp.es>
2017-06-19 02:06:21 +0200

    repeated code to sidebar template

--- 404.php
+++ 404.php
@@ -1,37 +1,8 @@
 <?php get_header(); ?>
 
-<?php
-$terms = get_terms( array(
-    'taxonomy' => 'category',
-    'hide_empty' => false,
-));
-?>
-
 <div class="page-post">
 
-  <aside class="sidebar">
-    <p><strong>01. B</strong>ienvenido</p>
-    <div class="container-img-welcome">
-      <img class="img-welcome" src="<?php echo get_template_directory_uri(); ?>/assets/img/img-post/img1.png" alt="">
-    </div>
-    <p><strong>02. C</strong>ategorías</p>
-
-    <ul class="list-categories">
-
-      <?php foreach ( $terms as $term ) { ?>
-        <?php if($term->term_id == 1) continue; //No mostrar categoría ?>
-          <li>
-            <a href="<?php echo get_term_link($term,'category');?>">
-              <strong><?php echo substr($term->name, 0, 1);?></strong><?php echo substr($term->name, 1);?>
-            </a> <img class="line" src="<?php echo get_template_directory_uri(); ?>/assets/img/line.svg" alt="">
-          </li>
-      <?php } ?>
-    </ul>
-
-    <p><strong>03. M</strong>ás populares</p>
-    <?php if (function_exists('wpp_get_mostpopular')) wpp_get_mostpopular(array('limit' => 3)); ?>
-
-  </aside>
+  <?php get_sidebar(); ?>
 
   <div class="current-post">
     <?php if (have_posts()) { ?>
--- single-receta.php
+++ single-receta.php
@@ -1,41 +1,8 @@
 <?php get_header(); ?>
 
-<?php
-$terms = get_terms( array(
-    'taxonomy' => 'category',
-    'hide_empty' => false,
-));
-?>
-
 <div class="page-post">
 
-  <aside class="sidebar">
-    <p><strong>01. B</strong>ienvenido</p>
-    <div class="container-img-welcome">
-      <img class="img-welcome" src="<?php echo get_template_directory_uri(); ?>/assets/img/img-post/img1.png" alt="">
-    </div>
-    <p><strong>02. C</strong>ategorías</p>
-
-    <ul class="list-categories">
-
-      <?php foreach ( $terms as $term ) { ?>
-        <?php if($term->term_id == 1) continue; //No mostrar categoría ?>
-          <li>
-            <a href="<?php echo get_term_link($term,'category');?>">
-              <strong><?php echo substr($term->name, 0, 1);?></strong><?php echo substr($term->name, 1);?>
-            </a> <img class="line" src="<?php echo get_template_directory_uri(); ?>/assets/img/line.svg" alt="">
-          </li>
-      <?php } ?>
-    </ul>
-
-    <p><strong>03. M</strong>ás populares</p>
-    <?php if (function_exists('wpp_get_mostpopular')) { ?>
-      <?php wpp_get_mostpopular(array('limit' => 3)); ?>
-    <?php }else{ ?>
-      Es necesario instalar plugin <a href="https://es.wordpress.org/plugins/contact-form-7/">Contact Form 7</a>
-    <?php } ?>
-
-  </aside>
+  <?php get_sidebar(); ?>
 
   <?php if (have_posts()) { ?>
     <?php while (have_posts()) { the_post(); ?>
